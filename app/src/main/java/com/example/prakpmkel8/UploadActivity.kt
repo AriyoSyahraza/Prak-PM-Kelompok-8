09dc9861c44f40d3e558e76710178d1d0a794c94
AriyoSyahraza <m45syahraza@gmail.com>
2023-12-21 10:04:18 +0700

    Ini Upload dan Update yang di upload oleh Fayadh dan Maria

---
+++ app/src/main/java/com/example/prakpmkel8/UploadActivity.kt
@@ -0,0 +1,50 @@
+package com.example.prakpmkel8
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.prakpmkel8.databinding.ActivityUploadBinding
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+
+class UploadActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityUploadBinding
+    private lateinit var database: DatabaseReference
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityUploadBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Initialize database reference only once in onCreate
+        database = FirebaseDatabase.getInstance().getReference("Users")
+
+        binding.saveButton.setOnClickListener {
+            val name = binding.uploadName.text.toString()
+            val operator = binding.uploadOperator.text.toString()
+            val location = binding.uploadLocation.text.toString()
+            val phone = binding.uploadPhone.text.toString()
+
+            val users = User(name, operator, location, phone)
+
+            // Use the initialized database reference here
+            database.child(phone).setValue(users).addOnSuccessListener {
+                binding.uploadName.text.clear()
+                binding.uploadOperator.text.clear()
+                binding.uploadLocation.text.clear()
+                binding.uploadPhone.text.clear()
+
+                Toast.makeText(this, "Saved", Toast.LENGTH_SHORT).show()
+
+                val intent = Intent(this@UploadActivity, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+
+            }.addOnFailureListener {
+                Toast.makeText(this, "Failed", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
